c206a706dc461e4b347948b7bdd737c558603fb5
Aries Dimas Yudhistira <alhusna901@gmail.com>
2018-02-16 07:53:05 +0700

    update database relation

--- yudhistira.id/yudhistira_db.sql
+++ yudhistira.id/yudhistira_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 16, 2018 at 12:46 AM
+-- Generation Time: Feb 16, 2018 at 01:52 AM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
